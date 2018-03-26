52d7fdacc7879930b3bbd8701dd88e62c7308359
shinriyo <shinriyo@gmail.com>
2018-03-26 22:08:37 +0900

    html and tsc file open

--- src/extension.ts
+++ src/extension.ts
@@ -11,14 +11,33 @@ export function activate(context: vscode.ExtensionContext) {
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "ionic-switch" is now active!');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
+    // open .html file.
+    let disposable = vscode.commands.registerCommand('extension.ionSwitchHTML', () => {
         vscode.window.showInformationMessage('Hello World!');
+        let textEditor = vscode.window.activeTextEditor;
+        if (textEditor) {
+            const fullPath: string = textEditor.document.fileName;
+            // open
+            const htmlFile: string = fullPath.replace(/^(\.tsc)/g, ".html");
+            vscode.workspace.openTextDocument(htmlFile).then(function(TextDocument){
+                vscode.window.showTextDocument(TextDocument, vscode.ViewColumn.One, false);
+            });
+        }
+    });
+
+    context.subscriptions.push(disposable);
+
+    // open .tsc file.
+    disposable = vscode.commands.registerCommand('extension.ionSwitchTSC', () => {
+        let textEditor = vscode.window.activeTextEditor;
+        if (textEditor) {
+            const fullPath: string = textEditor.document.fileName;
+            // open
+            const tscFile: string = fullPath.replace(/^(\.html)/g, ".tsc");
+            vscode.workspace.openTextDocument(tscFile).then(function(TextDocument){
+                vscode.window.showTextDocument(TextDocument, vscode.ViewColumn.One, false);
+            });
+        }
     });
 
     context.subscriptions.push(disposable);
